0073ca01b1edff77c4dd8cf1524cc9843f2799b7
Adam Farizi <adamfarizi4@gmail.com>
2023-11-27 14:50:54 +0700

    update cilik

--- resources/views/auth/pengguna/edit/edit_admin.blade.php
+++ resources/views/auth/pengguna/edit/edit_admin.blade.php
@@ -92,7 +92,7 @@
             <nav aria-label="breadcrumb">
                 <ol class="breadcrumb bg-transparent mb-0 pb-0 pt-1 px-0 me-sm-6 me-5">
                     <li class="breadcrumb-item text-sm"><a class="opacity-5 text-dark" href="javascript:;">Pages</a></li>
-                    <li class="breadcrumb-item text-sm"><a class="opacity-5 text-dark">Pelanggan</a></li>
+                    <li class="breadcrumb-item text-sm"><a class="opacity-5 text-dark">Pengguna</a></li>
                     <li class="breadcrumb-item text-sm text-dark active" aria-current="page">Edit Admin</li>                                                </ol>
                 </ol>
                 <h6 class="font-weight-bolder mb-0">Admin</h6>
