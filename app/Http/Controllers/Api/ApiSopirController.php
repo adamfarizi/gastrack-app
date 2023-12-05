a370b258867a037942ef4bb6b7b0b96f74184f9b
vandkurnia <vandkurnia@gmail.com>
2023-12-05 13:23:20 +0700

    up

--- app/Http/Controllers/Api/ApiSopirController.php
+++ app/Http/Controllers/Api/ApiSopirController.php
@@ -4,7 +4,9 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\Sopir;
+use App\Models\Pengiriman;
 use App\Http\Resources\PostResource;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class ApiSopirController extends Controller
@@ -70,6 +72,42 @@ class ApiSopirController extends Controller
                 'success' => false,
                 'message' => 'Gagal logout. Pengguna tidak ditemukan.',
             ], 401);
+        }
+    }
+
+    public function getDataPengiriman(string $id){
+        Carbon::setLocale('id');
+        $pengiriman = Pengiriman::where('id_sopir', $id)
+        ->join('pesanan', 'pengiriman.id_pesanan', '=', 'pesanan.id_pesanan')
+        ->join('transaksi', 'pesanan.id_transaksi', '=', 'transaksi.id_transaksi')
+        ->join('pelanggan', 'transaksi.id_pelanggan', '=', 'pelanggan.id_pelanggan');
+
+        $data = $pengiriman
+        ->select(
+            'transaksi.resi_transaksi AS resi',
+            'pelanggan.koordinat',
+            'pelanggan.nama_perusahaan',
+            'pelanggan.alamat AS alamat_perusahaan',
+            'pesanan.jumlah_pesanan',
+            'pesanan.tanggal_pesanan AS tanggal_pemesanaan'
+        )->first();
+
+        $cek_data = Pengiriman::where('id_sopir', $id)->first();
+    
+        if (empty($cek_data)) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Data tidak ditemukan!',
+            ], 422);
+        }
+        else{
+            $formattedTanggal = Carbon::parse($data->tanggal_pemesanaan)->isoFormat('DD MMMM YYYY');
+            $data->tanggal_pemesanaan = $formattedTanggal;
+            return response()->json([
+                'success' => true,
+                'message' => 'Data berhasil ditemukan',
+                'data' => $data,
+            ], 200);
         }
     }
 }
